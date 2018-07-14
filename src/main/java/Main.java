91a3e6479ae584b464ed33b6dbc2549bd62df67a
vladislav.fedotov <java.lang@yande.xru>
2018-07-14 18:39:06 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public long calculateWaterAmount(int[] landscape) {
+        return -1;
+    }
+}
